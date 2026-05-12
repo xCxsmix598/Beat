8af3dfe85e1f85e04f4daf9edc385ced10c21649
xCxsmix598 <xcosmic598@proton.me>
2026-05-12 23:18:45 +0800

    updated gitignore

--- start.py
+++ start.py
@@ -1,23 +1,6 @@
 import subprocess
-import signal
-import sys
-import time
 
-lavalink = subprocess.Popen(['java', '-jar', 'Lavalink.jar'], cwd='Lavalink/')
-bot = subprocess.Popen(['python', 'main.py'])
-
-
-def stopBot():
-    lavalink.send_signal(signal.SIGINT)
-    bot.send_signal(signal.SIGINT)
-    print('Program Exited')
-    sys.exit(0)
-
-
-x = 1
-while True:
-    try:
-        time.sleep(120)
-        x += 1
-    except KeyboardInterrupt:
-        stopBot()
+subprocess.Popen(['java', '-jar', '/home/jstxn/Projects/Beat/Beat-release/Lavalink/Lavalink.jar'],
+                 cwd='/home/jstxn/Projects/Beat/Beat-release/Lavalink/')
+subprocess.Popen(['python', 'main.py'],
+                 cwd='/home/jstxn/Projects/Beat/Beat-release/')
