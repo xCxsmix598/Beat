0119415cd3c60f4252fc056f52ff78fe7a57eaca
xCxsmix598 <xcosmic598@proton.me>
2026-05-12 23:07:46 +0800

    Second Commit

--- start.py
+++ start.py
@@ -1,6 +1,23 @@
 import subprocess
+import signal
+import sys
+import time
 
-subprocess.Popen(['java', '-jar', '/home/jstxn/Projects/Beat/Beat-release/Lavalink/Lavalink.jar'],
-                 cwd='/home/jstxn/Projects/Beat/Beat-release/Lavalink/')
-subprocess.Popen(['python', 'main.py'],
-                 cwd='/home/jstxn/Projects/Beat/Beat-release/')
+lavalink = subprocess.Popen(['java', '-jar', 'Lavalink.jar'], cwd='Lavalink/')
+bot = subprocess.Popen(['python', 'main.py'])
+
+
+def stopBot():
+    lavalink.send_signal(signal.SIGINT)
+    bot.send_signal(signal.SIGINT)
+    print('Program Exited')
+    sys.exit(0)
+
+
+x = 1
+while True:
+    try:
+        time.sleep(120)
+        x += 1
+    except KeyboardInterrupt:
+        stopBot()
